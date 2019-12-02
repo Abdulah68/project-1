f45967119978d4d2aed88dd2d5fdd4e1b3bdddf2
Abdulah68 <abdulahali6565@gmail.com>
2019-12-02 17:10:09 +0300

    second day

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,84 @@
 $(function(){
     let count = 0;
-    $('.box').on('click',function(event){
-        count++;
+    function game(){
         const element =  $(event.target)
-       if(count %3 ==1){
-        
+       if(count % 2 === 0){
         $(element).text('x')
         $(element).off("click")
-       }
-       else if (count %2 == 0)
-       {
+       }else{
         $(element).text('o')
         $(element).off("click")
+        }
+       count++;
+       checkGame()
+    }
+       
+     $('.box').on('click',game)
+       function checkGame(){
+       if($('#0').text()  == $('#1').text() && $('#2').text() == $('#1').text() && $('#2').text() != "")
+        {
+        setTimeout(function() {alert(  `player ${$('#0').text()}  is win` )},1000)
+          rest()
+        }
+        if($('#3').text()  == $('#4').text() && $('#5').text() == $('#4').text() && $('#5').text() != "")
+        {
+        setTimeout(function(){ alert(  `player ${$('#3').text()}  is win` )},1000)
+          rest()
+        }
+        if($('#6').text()  == $('#7').text() && $('#8').text() == $('#7').text() && $('#8').text() != "")
+        {
+         setTimeout(function()  {alert(  `player ${$('#6').text()}  is win` )},1000)
+         rest()
+        }
+         if($('#0').text()  == $('#3').text() && $('#6').text() == $('#3').text() && $('#6').text() != "")
+        {
+         setTimeout(function()  {alert(  `player ${$('#0').text()}  is win` )},1000)
+         rest()
+        }
+         if($('#1').text()  == $('#4').text() && $('#7').text() == $('#4').text() && $('#7').text() != "")
+        {
+        setTimeout(function()  { alert(  `player ${$('#1').text()}  is win` )},1000)
+            rest()
+        }
+        if($('#2').text()  == $('#5').text() && $('#8').text() == $('#5').text() && $('#8').text() != "")
+        {
+        setTimeout(function() { alert(  `player ${$('#2').text()}  is win` )},1000)
+           rest()
+        }
+         if($('#0').text()  == $('#4').text() && $('#8').text() == $('#4').text() && $('#8').text() != "")
+        {
+         setTimeout(function() { alert(  `player ${$('#0').text()}  is win` )},1000)
+           rest()
+        }
+        if($('#2').text()  == $('#4').text() && $('#6').text() == $('#4').text() && $('#6').text() != "")
+       {
+         setTimeout(function() {alert(  `player ${$('#2').text()}  is win` )},1000)
+           rest()
        }
+      else  if (count == 9 ) 
+       {
+          setTimeout(function()  {alert(  `player is tie` )},1000)
+          rest()
+       }
+    }
+      function rest(){
+         count =0
+         $(".box").html("")
+         $(".box").off()
+         $(".box").on('click', game)
+       }
+       $("#i1").on("click", rest)
+            
+      
+    
+
+        
        
-       
-       
+    
+
  
-    })
-   
+ 
+});
 
 
 
-
-
-
-
-})
